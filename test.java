5472d232190f80e161a70727ae75b41c6031db0e
SalimNoori <salimshah.noori@seattlecolleges.edu>
2024-06-25 10:17:54 -0700

    Code update

--- test.java
+++ test.java
@@ -5,8 +5,10 @@
 
 import java.util.Scanner;
 
-public class HelloWorld {
-    public static void main(String[] args) {
+public class HelloWorld 
+{
+    public static void main(String[] args) 
+    {
         Scanner s = new Scanner(System.in);
         
         System.out.print("Please enter your name: ");
