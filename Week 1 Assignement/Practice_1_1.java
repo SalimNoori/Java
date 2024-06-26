c2049b703a069546059e5c7f22fccb6f73ceb2b7
SalimNoori <salimshah.noori@seattlecolleges.edu>
2024-06-26 13:38:18 -0700

    Changes to outline

--- Week 1 Assignement/Practice_1_1.java
+++ Week 1 Assignement/Practice_1_1.java
@@ -1,11 +1,11 @@
 public class Practice_1_1 {
     public static void main(String[] args) {
         // Define variables to store personal information
-        String name = "Salim Noori"; // Replace with your actual name
-        String birthdate = "Nov 24"; // Replace with your actual birthdate
-        String hobbies = "Soccer, Hiking"; // Replace with your actual hobbies
-        String favoriteBook = "Think And Growth Rich"; // Replace with your favorite book
-        String favoriteMovie = "Avatar"; // Replace with your favorite movie
+        String name = "Your Name"; // Replace with your actual name
+        String birthdate = "Your Birthdate"; // Replace with your actual birthdate
+        String hobbies = "Your Hobbies"; // Replace with your actual hobbies
+        String favoriteBook = "Your Favorite Book"; // Replace with your favorite book
+        String favoriteMovie = "Your Favorite Movie"; // Replace with your favorite movie
 
         // Print each piece of information with labels
         System.out.println("Name: " + name); // Prints your name
