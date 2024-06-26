c4d486a9c17fda4995cb0fde37dbafdf8434694d
SalimNoori <salimshah.noori@seattlecolleges.edu>
2024-06-26 13:53:29 -0700

    Code updated

---
+++ Week 1 Assignement/Practice_1_2.java
@@ -0,0 +1,14 @@
+public class Practice_1_2 {
+    public static void main(String[] args) {
+        // Replace "YourFirstName" with your actual first name
+        // Replace "YourLastName" with your actual last name
+        String firstName = "YourFirstName";
+        String lastName = "YourLastName";
+
+        // Get the initial of the last name
+        char lastNameInitial = lastName.charAt(0);
+
+        // Print out the result
+        System.out.println("Initial is " + firstName.charAt(0) + lastNameInitial);
+    }
+}
