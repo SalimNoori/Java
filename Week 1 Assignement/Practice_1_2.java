36648fc308a8d75859e13883672d7fda5ad7926f
SalimNoori <salimshah.noori@seattlecolleges.edu>
2024-06-26 13:57:02 -0700

    code changed

--- Week 1 Assignement/Practice_1_2.java
+++ Week 1 Assignement/Practice_1_2.java
@@ -1,10 +1,15 @@
 public class Practice_1_2 {
     public static void main(String[] args) {
+        // Define the first name
         String firstName = "Salim";
+        // Define the last name
         String lastName = "Noori";
 
+        // Get the initial of the last name (first character of the last name)
         char lastNameInitial = lastName.charAt(0);
 
+        // Print out the initials by combining the first character of the first name
+        // and the first character of the last name
         System.out.println("Initial is " + firstName.charAt(0) + lastNameInitial);
     }
 }
